16eaefcd31d5ce968a7a983caaac5502c59d18d6
nguyendinhkhiem123 <nguyendinhkhiem12a4@gmail.com>
2021-09-19 16:08:55 +0700

    Khiêm nè

--- thuc-tap-4/src/App.js
+++ thuc-tap-4/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello reactjs</code> khiêm nè.
+          Edit <code>Nguyễn Đinhf khiêm</code> khiêm nè.
         </p>
         <a
           className="App-link"
